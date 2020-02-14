e1cca6800690364851454b858b816d2e95e474b6
NSALMAN <nathierfouad@gmail.com>
2020-02-14 10:39:25 +0100

    Initial commit

---
+++ src/Assessment/Book.java
@@ -0,0 +1,57 @@
+package Assessment;
+
+public class Book {
+    private String name;
+    private double price;
+    private int quantity;
+    private Author author;
+    private int year;
+
+    public Book(String name, double price, int quantity, Author author, int year) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ src/Assessment/Main.java
@@ -0,0 +1,16 @@
+package Assessment;
+
+public class Main {
+
+    public static void main(String[] args) {
+           Author author=new Author("koen","koen.griffion@capgemini",26,"m");
+           Author author1=new Author("Alice Munrno","Alice@gmial.com",88,"f");
+           Book book=new Book("select Stories",16.99,150000000,author,1996);
+           Book book1=new Book("The Moon Of Jupiter",14.99,1000000,author1,1988);
+           Book koen=new Book("thoughts on java",1.99,1,author,2020);
+           Book koen1=new Book("Thoughts on Dotnet",4.99, 1,author,2020);
+        System.out.println("Author:"+author+ "\n Author1:"+author1);
+        System.out.println("book:"+book+"\n Book:"+book1);
+
+    }
+}
